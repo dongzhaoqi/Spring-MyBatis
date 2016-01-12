c655a5f03d05749edd3ad34e3e3864896b8225af
Dong <Dong@Dong-PC>
2016-01-12 12:23:12 +0800

    Added readConsoleToFile() 2016-1-12

--- src/com/dong/util/CommonUtils.java
+++ src/com/dong/util/CommonUtils.java
@@ -1,9 +1,14 @@
 package com.dong.util;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -14,11 +19,16 @@ public class CommonUtils {
 		ArrayList<String>arrayList = new ArrayList<String>();
 		arrayList = stringToArrayList(str);
 		System.out.println(arrayList);
+		
+		
 		str = ArrayListToString(arrayList);
 		System.out.println(str);
 		
 		
 		readFileToConsole("D:/logs/2016-01-08.log");
+		
+		
+		readConsoleToFile("D:/logs/test.log");
 	}
 	
 	
@@ -58,6 +68,7 @@ public class CommonUtils {
 	/**
 	 * Read local file to console
 	 * @param filePath
+	 * @date 2016-1-12
 	 */
 	public static void readFileToConsole(String filePath){
 		
@@ -75,6 +86,31 @@ public class CommonUtils {
 			e.printStackTrace();
 		}
 		
+	}
+	
+	/**
+	 * Read concole content to file
+	 * @param filePath
+	 * @date 2016-1-12
+	 */
+	public static void readConsoleToFile(String filePath){
+		File file = new File(filePath);
+		if(!file.exists()){
+			try {
+				file.createNewFile();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			System.out.println("文件"+file.getAbsolutePath()+"已创建");
+		}
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		try {
+			String content = reader.readLine();
+			OutputStream outputStream = new FileOutputStream(file);
+			outputStream.write(content.getBytes());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 }
